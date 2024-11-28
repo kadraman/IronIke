1055068f7c49ffd813251e8de67258dfa4370ea3
kadraman <klee2@opentext.com>
2024-11-28 17:07:47 +0000

    Basic L1 Large Map

--- include/GlobalVars.h
+++ include/GlobalVars.h
@@ -4,10 +4,10 @@
 #define MAX_LIVES 3
 #define MAX_LEVEL 2
 
-#define TILE_ITEM_SPIKE	97
-#define TILE_ITEM_COIN	51
-#define TILE_ITEM_BOX	52
-#define TILE_ITEM_FLAG	125
+#define TILE_ITEM_SPIKE	200
+#define TILE_ITEM_COIN	201
+#define TILE_ITEM_BOX	202
+#define TILE_ITEM_FLAG	203
 
 extern UINT16 g_player_score;
 extern UINT8 g_level_current;
